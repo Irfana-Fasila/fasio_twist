3d7e8fe76eca4b7c5d37afb07596e83cdb5ae7a4
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-03-08 23:11:02 +0530

    update api.dart and category_section.dart

--- lib/screens/home/components/categories/category_section.dart
+++ lib/screens/home/components/categories/category_section.dart
@@ -2,7 +2,6 @@ import 'package:fasio_twist/view_model/auth_vm.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-// Define the categories provider with age-based filtering
 final categoriesProvider = Provider<Map<String, Map<String, dynamic>>>((ref) {
   final int age = ref.watch(authVM).age ?? 0;
 
