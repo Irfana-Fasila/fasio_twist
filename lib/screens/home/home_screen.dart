2040a1ee9e0f334be4245a31f4a4c9d08d2b4bec
Fazil <104255104+fazil-kp@users.noreply.github.com>
2025-03-09 18:11:10 +0530

    update 9 files

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -24,6 +24,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         backgroundColor: Colors.white,
         elevation: 0,
         title: Text(
